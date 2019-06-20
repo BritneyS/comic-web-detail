7ce575113ba4cdf905655dd6336979ce69eac303
Britney Smith <brit.smith7@gmail.com>
2019-06-20 13:49:30 -0400

    Basic grommet example setup: header, body, sidebar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,58 @@
 import React from 'react';
+import { Box, Button, Heading, Grommet } from 'grommet';
+import { Notification } from 'grommet-icons';
+
+const theme = {
+  global: {
+    colors: {
+      brand: '#f44283'
+    },
+    font: {
+      family: 'Roboto',
+      size: '14px',
+      height: '20px'
+    },
+  },
+};
+
+const AppBar = (props) => (
+  <Box
+    tag='header'
+    direction='row'
+    align='center'
+    justify='between'
+    background='brand'
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation='medium'
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grommet theme={theme} full>
+      <Box fill>
+        <AppBar>
+          <Heading level='3' margin='none'>My App</Heading>
+          <Button icon={<Notification />} onClick={() => {}} />
+        </AppBar>
+        <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+          <Box flex align='center' justify='center'>
+            app body
+          </Box>
+          <Box
+            width='medium'
+            background='light-2'
+            elevation='small'
+            align='center'
+            justify='center'
+          >
+            sidebar
+          </Box>
+        </Box>
+      </Box>
+    </Grommet>
   );
 }
 
