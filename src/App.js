df5be2ad591d4b87d0b4bdcc96aa147a636dffbc
Britney Smith <brit.smith7@gmail.com>
2019-06-26 21:14:19 -0400

    Add responsive layer for mobile views

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { useState } from 'react';
-import { Box, Button, Collapsible, Heading, Grommet } from 'grommet';
-import { Notification } from 'grommet-icons';
+import { 
+  Box, 
+  Button, 
+  Collapsible,
+  Grommet, 
+  Heading, 
+  Layer,
+  ResponsiveContext 
+} from 'grommet';
+import { FormClose, Notification } from 'grommet-icons';
 
 const theme = {
   global: {
@@ -34,31 +42,58 @@ function App() {
 
   return (
     <Grommet theme={theme} full>
-      <Box fill>
-        <AppBar>
-          <Heading level='3' margin='none'>My App</Heading>
-          <Button icon={<Notification />} onClick={() => setShowSidebar(!showSideBar)} />
-        </AppBar>
-        <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
-          <Box flex align='center' justify='center'>
-            app body
-          </Box>
-          {showSideBar && (
-            <Collapsible direction="horizontal" open={showSideBar}>
-              <Box
-                flex
-                width='medium'
-                background='light-2'
-                elevation='small'
-                align='center'
-                justify='center'
-            >
-              sidebar
+      <ResponsiveContext.Consumer>
+        {size => (
+          <Box fill>
+            <AppBar>
+              <Heading level='3' margin='none'>My App</Heading>
+              <Button icon={<Notification />} onClick={() => setShowSidebar(!showSideBar)} />
+            </AppBar>
+            <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+              <Box flex align='center' justify='center'>
+                app body
+              </Box>
+              {(!showSideBar || size !== 'small') ? (
+                <Collapsible direction="horizontal" open={showSideBar}>
+                  <Box
+                    flex
+                    width='medium'
+                    background='light-2'
+                    elevation='small'
+                    align='center'
+                    justify='center'
+                >
+                  sidebar
+                </Box>
+              </Collapsible>
+              ) : (
+                <Layer>
+                  <Box
+                    background='light-2'
+                    tag='header'
+                    justify='end'
+                    align='center'
+                    direction='row'
+                  >
+                    <Button
+                      icon={<FormClose />}
+                      onClick={() => setShowSidebar(false)}
+                    />
+                  </Box>
+                  <Box
+                    fill
+                    background='light-2'
+                    align='center'
+                    justify='center'
+                  >
+                    sidebar
+                  </Box>
+                </Layer>
+              )}
             </Box>
-          </Collapsible>
-          )}
-        </Box>
-      </Box>
+          </Box>
+        )}
+      </ResponsiveContext.Consumer>
     </Grommet>
   );
 }
