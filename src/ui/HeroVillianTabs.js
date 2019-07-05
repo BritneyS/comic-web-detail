5846c1f15321999b923fd38c42b3af20c57e7519
Britney Smith <brit.smith7@gmail.com>
2019-07-05 14:21:11 -0400

    Add hero and villain fake array to tabs

--- src/ui/HeroVillianTabs.js
+++ src/ui/HeroVillianTabs.js
@@ -1,19 +1,63 @@
 import React from 'react';
-import { 
-// eslint-disable-next-line
+import {
   Box,
   Tab,
   Tabs,
-// eslint-disable-next-line
   Text,
 // eslint-disable-next-line
   Image 
 } from 'grommet';
 
+const heroes = [
+    {
+        name: 'batman'
+    },
+    {
+        name: 'superman'
+    },
+    {
+        name: 'deadpool'
+    }
+];
+
+const villains = [
+    {
+        name: 'joker'
+    },
+    {
+        name: 'mr. freeze'
+    },
+    {
+        name: 'the penguin'
+    }
+];
+
 const HeroVillianTabs = (props) => (
     <Tabs height='medium' flex='grow' alignSelf='left'>
-        <Tab title='Heroes'></Tab>
-        <Tab title='Villains'></Tab>
+        <Tab title='Heroes'>
+            {heroes.map (hero => {
+                return (
+                    <Box
+                    margin='small'
+                    pad='small'
+                >
+                    <Text>{hero.name}</Text>
+                </Box>
+                );
+            })}
+        </Tab>
+        <Tab title='Villains'>
+            {villains.map (villain => {
+                return (
+                    <Box
+                    margin='small'
+                    pad='small'
+                >
+                    <Text>{villain.name}</Text>
+                </Box>
+                );
+            })}
+        </Tab>
     </Tabs>
 );
 
